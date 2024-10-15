8566ebe2739d58771d98147d1528ffc064c3efc3
MuzammilAmjad <muzammilamjad217@gmail.com>
2024-10-15 21:54:23 +0500

    Functionality of Calculator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import {CalculatorService} from './app.component.service';
 
 @Component({
   selector: 'app-root',
@@ -7,20 +8,37 @@ import { FormControl, ReactiveFormsModule } from '@angular/forms';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   imports: [ReactiveFormsModule],
-
+  providers: [CalculatorService]
 })
 export class AppComponent {
   title = 'Calculator';
   display = new FormControl('0');
   currentValue = '0';
 
-  onNumberClick(value: string) {}
+  constructor(private calculatorService: CalculatorService) {}
 
-  onOperationClick(operation: string) {}
+  onNumberClick(value: string) {
+    this.currentValue = this.calculatorService.appendNumber(this.currentValue, value);
+    this.display.setValue(this.currentValue);
+  }
 
-  onEqualsClick() {}
+  onOperationClick(operation: string) {
+    this.currentValue = this.calculatorService.appendOperation(this.currentValue, operation);
+    this.display.setValue(this.currentValue);
+  }
 
-  onResetClick() {}
+  onEqualsClick() {
+    this.currentValue = this.calculatorService.calculate(this.currentValue);
+    this.display.setValue(this.currentValue);
+  }
 
-  onDeleteClick() {}
+  onResetClick() {
+    this.currentValue = '0';
+    this.display.setValue(this.currentValue);
+  }
+
+  onDeleteClick() {
+    this.currentValue = this.calculatorService.deleteLast(this.currentValue);
+    this.display.setValue(this.currentValue);
+  }
 }
