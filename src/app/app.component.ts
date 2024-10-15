49c9d08e3ac49081f34274a4a1c2a0e89d32f877
MuzammilAmjad <muzammilamjad217@gmail.com>
2024-10-15 16:23:44 +0500

    UI Design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  imports: [ReactiveFormsModule],
+
 })
 export class AppComponent {
-  title = 'CalculatorwithAngular';
+  title = 'Calculator';
+  display = new FormControl('0');
+  currentValue = '0';
+
+  onNumberClick(value: string) {}
+
+  onOperationClick(operation: string) {}
+
+  onEqualsClick() {}
+
+  onResetClick() {}
+
+  onDeleteClick() {}
 }
