a93da84d2c3e51db77b7f9b9be3a7fffbe2133cb
MuzammilAmjad <muzammilamjad217@gmail.com>
2024-10-18 00:23:08 +0500

    Pipes and make Calculator Component transfer code in that component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,44 +1,16 @@
 import { Component } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
-import {CalculatorService} from './app.component.service';
+import {CalculatorService} from './calculator/calculator.service';
+import {CustomNumberPipe} from './calculator/calculator.pipe';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule,CustomNumberPipe],
   providers: [CalculatorService]
 })
 export class AppComponent {
   title = 'Calculator';
-  display = new FormControl('0');
-  currentValue = '0';
-
-  constructor(private calculatorService: CalculatorService) {}
-
-  onNumberClick(value: string) {
-    this.currentValue = this.calculatorService.appendNumber(this.currentValue, value);
-    this.display.setValue(this.currentValue);
-  }
-
-  onOperationClick(operation: string) {
-    this.currentValue = this.calculatorService.appendOperation(this.currentValue, operation);
-    this.display.setValue(this.currentValue);
-  }
-
-  onEqualsClick() {
-    this.currentValue = this.calculatorService.calculate(this.currentValue);
-    this.display.setValue(this.currentValue);
-  }
-
-  onResetClick() {
-    this.currentValue = '0';
-    this.display.setValue(this.currentValue);
-  }
-
-  onDeleteClick() {
-    this.currentValue = this.calculatorService.deleteLast(this.currentValue);
-    this.display.setValue(this.currentValue);
-  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import {CalculatorComponent} from './app/calculator/calculator.component';
 
-bootstrapApplication(AppComponent, appConfig)
+bootstrapApplication(CalculatorComponent, appConfig)
   .catch((err) => console.error(err));
